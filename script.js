a31613d80c85a58ff417c7000f858c2cfd3fc5eb
Charity Quinsay <cquinsay@gmail.com>
2020-11-12 05:46:00 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+for(var i=1; i<20; i++){
+    if(i%2 == 1){
+        console.log(i);
+    }
+}
+
+var sum=0;
+for(var num=1; num<=5; num++){
+    sum+=num;
+    console.log(num);
+    console.log(sum);
+}
